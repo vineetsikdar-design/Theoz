dd7e2233665940d5c4d33ebcc91840633e532693
vineetsikdar-design <vineetsikdar@gmail.com>
2026-08-20 14:02:24 +0530

    Update ZentraxUI.h

--- ZentraxUI.h
+++ ZentraxUI.h
@@ -4,26 +4,34 @@
 //
 //  Created by Zentrax Team.
 //  Architecture: Ultra-Premium SaaS Layer
-//  Status: PRODUCTION READY (Strictly Dynamic Data)
+//  Status: PRODUCTION READY
 //
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger, ZXAuthError) {
+    ZXAuthErrorNone,
+    ZXAuthErrorInvalid,
+    ZXAuthErrorExpired,
+    ZXAuthErrorConnection,
+    ZXAuthErrorServer
+};
+
 /// Delegate protocol for backend network integration
 @protocol ZentraxUIDelegate <NSObject>
 @optional
-/// Fired when the user attempts to log in. Must call completion block with server response.
-- (void)zentraxDidRequestAuthenticationWithKey:(NSString *)key completion:(void(^)(BOOL success, NSString * _Nullable errorMsg))completion;
+/// Fired when the user attempts to log in
+- (void)zentraxDidRequestAuthenticationWithKey:(NSString *)key completion:(void(^)(BOOL success, ZXAuthError errorType, NSString * _Nullable errorMsg))completion;
 
-/// Fired when a module switch is toggled. `moduleId` is the exact name provided by the backend.
+/// Fired when a module switch is toggled
 - (void)zentraxDidRequestModuleToggle:(NSString *)moduleId state:(BOOL)isOn completion:(void(^)(BOOL success, NSString * _Nullable errorMsg))completion;
 
 /// Fired if a logout action is triggered
 - (void)zentraxDidRequestLogoutWithCompletion:(void(^)(void))completion;
 
-/// NEW: Silently verifies the existing Keychain session on app launch
+/// Silently verifies the existing Keychain session on app launch
 - (void)zentraxDidRequestSessionVerificationWithCompletion:(void(^)(BOOL isValid))completion;
 @end
 
@@ -43,7 +51,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)showSuccessMessage:(NSString *)title message:(NSString *)message;
 - (void)showGlobalErrorWithTitle:(NSString *)title message:(NSString *)msg;
 
-// MARK: - Dynamic Data Injection (Strictly populated by server response)
+// MARK: - Dynamic Data Injection
 /// Passes the array of module dictionaries received from the PHP backend to build the list
 - (void)updateDashboardWithModules:(NSArray<NSDictionary *> *)modules;
 
